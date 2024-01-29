a848dc00cb7e4a362bd16191a60c6ad329830534
andrew <andrew@mbp.lan>
2024-01-29 14:30:18 -0600

    passing test reading in metal files from separate dir of .metal files as apposed to wrapping them in mutli line char in .h files

--- my_extension/cpp_extension.mm
+++ my_extension/cpp_extension.mm
@@ -1,8 +1,28 @@
 #include <torch/extension.h>
-#include "add_tensors.h"
-
 #import <Foundation/Foundation.h>
 #import <Metal/Metal.h>
+
+#include <fstream>
+#include <sstream>
+#include <cstring>
+
+
+// function for reading in metal shader files as const char
+const char* read_metal_shader(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open Metal shader file: " + filename);
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string str = buffer.str();
+
+    char* shader_code = new char[str.length() + 1];
+    std::strcpy(shader_code, str.c_str());
+    return shader_code;
+}
+
 
 // Helper function to retrieve the `MTLBuffer` from a `torch::Tensor`.
 static inline id<MTLBuffer> getMTLBufferStorage(const torch::Tensor& tensor) {
@@ -20,10 +40,17 @@ torch::Tensor& dispatchAddTensors(const torch::Tensor& a,
         // Set the number of threads equal to the number of elements within the input tensor.
         int numThreads = a.numel();
 
+        // get the shader source code as
+        const char* CUSTOM_KERNEL = read_metal_shader("./my_extension/metal/add_tensors.metal");
+
         // Load the custom soft shrink shader.
         id<MTLLibrary> customKernelLibrary = [device newLibraryWithSource:[NSString stringWithUTF8String:CUSTOM_KERNEL]
                                                                   options:nil
                                                                     error:&error];
+        // free the memory from const char
+        delete[] CUSTOM_KERNEL;
+
+        // check the library was created with CUSTOM_KERNEL
         TORCH_CHECK(customKernelLibrary, "Failed to to create custom kernel library, error: ", error.localizedDescription.UTF8String);
 
         std::string kernel_name = "addTensors";
