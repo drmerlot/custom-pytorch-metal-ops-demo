c9bfe6aff6df34634b21560944c279e48598d1f7
andrew <andrew@mbp>
2024-02-01 20:20:17 -0600

    working pytorch level custom example with MPS metal kl kernel replaced

--- my_extension/wrapper.py
+++ my_extension/wrapper.py
@@ -10,50 +10,81 @@ def add_tensors(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     return my_extension_cpp.add_tensors(a, b)
 
 
+# class CustomLinear(nn.Module):
+#     def __init__(self, input_features, output_features):
+#         super(CustomLinear, self).__init__()
+#         self.input_features = input_features
+#         self.output_features = output_features
+#         self.weight = nn.Parameter(torch.Tensor(output_features, input_features))
+#         self.reset_parameters()
+
+#     def reset_parameters(self):
+#         nn.init.kaiming_uniform_(self.weight)
+
+#     def forward(self, inp):
+#         inp_contiguous = inp.contiguous()
+#         return CustomLinearFunction.apply(inp_contiguous, self.weight)
+
+#     def extra_repr(self):
+#         return 'input_features={}, output_features={}'.format(
+#             self.input_features, self.output_features
+#         )
+
+
+# class CustomLinearFunction(Function):
+#     @staticmethod
+#     def forward(ctx, inp, weight):
+#         ctx.save_for_backward(inp, weight)
+#         inp_cont = inp.contiguous()
+#         weight_cont = weight.contiguous()
+#         return my_extension_cpp.matrix_multiply(inp_cont, weight_cont.t())
+
+#     @staticmethod
+#     def backward(ctx, grad_output):
+#         inp, weight = ctx.saved_tensors
+#         grad_input = grad_weight = None
+
+#         if ctx.needs_input_grad[0]:
+#             # Gradient with respect to input
+#             grad_input = my_extension_cpp.matrix_multiply(grad_output, weight.t())
+
+#         if ctx.needs_input_grad[1]:
+#             # Gradient with respect to weight
+#             inp_t = inp.t().contiguous()
+#             grad_output = grad_output.contiguous()
+#             grad_weight = my_extension_cpp.matrix_multiply(inp_t, grad_output)
+#         return grad_input, grad_weight
+
 class CustomLinear(nn.Module):
-    def __init__(self, input_features, output_features):
+    def __init__(self, in_features, out_features):
         super(CustomLinear, self).__init__()
-        self.input_features = input_features
-        self.output_features = output_features
-        self.weight = nn.Parameter(torch.Tensor(output_features, input_features))
-        self.reset_parameters()
+        self.weight = nn.Parameter(torch.randn(out_features, in_features))
 
-    def reset_parameters(self):
-        nn.init.kaiming_uniform_(self.weight)
-
-    def forward(self, inp):
-        inp_contiguous = inp.contiguous()
-        return CustomLinearFunction.apply(inp_contiguous, self.weight)
-
-    def extra_repr(self):
-        return 'input_features={}, output_features={}'.format(
-            self.input_features, self.output_features
-        )
+    def forward(self, x):
+        return CustomLinearFunction.apply(x, self.weight)
 
 
 class CustomLinearFunction(Function):
     @staticmethod
-    def forward(ctx, inp, weight):
-        ctx.save_for_backward(inp, weight)
-        inp_cont = inp.contiguous()
-        weight_cont = weight.contiguous()
-        return my_extension_cpp.matrix_multiply(inp_cont, weight_cont.t())
+    def forward(ctx, input, weights):
+        ctx.save_for_backward(input, weights)
+        output = input @ weights.t()
+        return output
 
     @staticmethod
     def backward(ctx, grad_output):
-        inp, weight = ctx.saved_tensors
-        grad_input = grad_weight = None
+        input, weights = ctx.saved_tensors
+
+        grad_inputs = grad_weights = None
 
         if ctx.needs_input_grad[0]:
-            # Gradient with respect to input
-            grad_input = my_extension_cpp.matrix_multiply(grad_output, weight.t())
+            grad_inputs = grad_output @ weights  # [N, out_features] @ [in_features, out_features].T -> [N, in_features]
 
         if ctx.needs_input_grad[1]:
-            # Gradient with respect to weight
-            inp_t = inp.t().contiguous()
-            grad_output = grad_output.contiguous()
-            grad_weight = my_extension_cpp.matrix_multiply(inp_t, grad_output)
-        return grad_input, grad_weight
+            grad_weights = input.t() @ grad_output  # [in_features, N].T @ [N, out_features] -> [in_features, out_features]
+            grad_weights = grad_weights.t()
+
+        return grad_inputs, grad_weights
 
 
 class CustomReLU(nn.Module):
