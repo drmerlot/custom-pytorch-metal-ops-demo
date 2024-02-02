c427263375788ff71f9e32815c7c429aad4b733f
drmerlot <andrewsommerlot@gmail.com>
2024-02-02 10:55:37 -0600

    cleaned up some code

--- my_extension/wrapper.py
+++ my_extension/wrapper.py
@@ -10,51 +10,6 @@ def add_tensors(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     return my_extension_cpp.add_tensors(a, b)
 
 
-# class CustomLinear(nn.Module):
-#     def __init__(self, input_features, output_features):
-#         super(CustomLinear, self).__init__()
-#         self.input_features = input_features
-#         self.output_features = output_features
-#         self.weight = nn.Parameter(torch.Tensor(output_features, input_features))
-#         self.reset_parameters()
-
-#     def reset_parameters(self):
-#         nn.init.kaiming_uniform_(self.weight)
-
-#     def forward(self, inp):
-#         inp_contiguous = inp.contiguous()
-#         return CustomLinearFunction.apply(inp_contiguous, self.weight)
-
-#     def extra_repr(self):
-#         return 'input_features={}, output_features={}'.format(
-#             self.input_features, self.output_features
-#         )
-
-
-# class CustomLinearFunction(Function):
-#     @staticmethod
-#     def forward(ctx, inp, weight):
-#         ctx.save_for_backward(inp, weight)
-#         inp_cont = inp.contiguous()
-#         weight_cont = weight.contiguous()
-#         return my_extension_cpp.matrix_multiply(inp_cont, weight_cont.t())
-
-#     @staticmethod
-#     def backward(ctx, grad_output):
-#         inp, weight = ctx.saved_tensors
-#         grad_input = grad_weight = None
-
-#         if ctx.needs_input_grad[0]:
-#             # Gradient with respect to input
-#             grad_input = my_extension_cpp.matrix_multiply(grad_output, weight.t())
-
-#         if ctx.needs_input_grad[1]:
-#             # Gradient with respect to weight
-#             inp_t = inp.t().contiguous()
-#             grad_output = grad_output.contiguous()
-#             grad_weight = my_extension_cpp.matrix_multiply(inp_t, grad_output)
-#         return grad_input, grad_weight
-
 class CustomLinear(nn.Module):
     def __init__(self, in_features, out_features):
         super(CustomLinear, self).__init__()
@@ -122,6 +77,4 @@ class CustomReLUFunction(Function):
         # Create gradient tensor, only allowing gradients to flow where input > 0
         grad_input = grad_output.clone()
         grad_input[inp < 0.0] = 0.0
-        #grad_output = grad_output.contiguous()
-        #grad_input = torch.where(inp > 0, grad_output, 0.01 * grad_output)
         return grad_input
